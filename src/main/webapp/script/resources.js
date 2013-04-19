ea4b4eabb7053301268fc8270696672ad24cd237
tam <tamsayers@hotmail.com>
2013-04-19 15:09:31 +0100

    added url response output to view

--- src/main/webapp/script/resources.js
+++ src/main/webapp/script/resources.js
@@ -1,19 +1,31 @@
 function Resource(data) {
 	var self = this
-	
+
 	self.url = data.url
 	self.types = data.types
 }
 
 function ResourcesViewModel() {
 	var self = this
-	
+
 	self.resources = ko.observableArray([])
-	
+
 	$.getJSON("/rest/content/list", function(allData) {
-        var mappedResources = $.map(allData.resources, function(item) { return new Resource(item) });
-        self.resources(mappedResources);
-    });  
+		var mappedResources = $.map(allData.resources, function(item) {
+			return new Resource(item)
+		})
+		self.resources(mappedResources)
+	})
+
+	self.showContent = function(resource) {
+		$.ajax(resource.url, {
+			headers: {'Accept':resource.types[0]},
+			dataType: 'text',
+			success: function(data) {
+				alert(data.toString())
+			}
+		})
+	}
 }
 
 ko.applyBindings(new ResourcesViewModel())
